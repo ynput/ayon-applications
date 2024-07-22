b6832e718d469013477cf50013f8dc059eac0fc1
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-07-22 17:15:50 +0200

    added enumerators for applications and tools

--- server/settings.py
+++ server/settings.py
@@ -2,6 +2,7 @@ import os
 import json
 from pydantic import validator
 
+from ayon_server.addons import BaseServerAddon
 from ayon_server.settings import (
     BaseSettingsModel,
     SettingsField,
@@ -15,6 +16,32 @@ ICONS_DIR = os.path.join(
     "public",
     "icons"
 )
+DEFAULT_APP_GROUPS = {
+    "maya",
+    "adsk_3dsmax",
+    "flame",
+    "nuke",
+    "nukeassist",
+    "nukex",
+    "nukestudio",
+    "hiero",
+    "fusion",
+    "resolve",
+    "houdini",
+    "blender",
+    "harmony",
+    "tvpaint",
+    "photoshop",
+    "aftereffects",
+    "celaction",
+    "substancepainter",
+    "unreal",
+    "wrap",
+    "openrv",
+    "zbrush",
+    "equalizer",
+    "motionbuilder",
+}
 
 
 def icons_enum():
@@ -24,6 +51,109 @@ def icons_enum():
     ]
     icons.insert(0, {"label": "None", "value": ""})
     return icons
+
+
+async def applications_enum(
+    project_name: str | None = None,
+    addon: BaseServerAddon = None,
+    settings_variant: str = None,
+):
+    if project_name is None:
+        settings = await addon.get_studio_settings(variant=settings_variant)
+    else:
+        settings = await addon.get_project_settings(
+            project_name=project_name, variant=settings_variant
+        )
+
+    apps_settings = settings.applications
+    apps_fields = apps_settings.__fields__
+    apps_groups = set(apps_fields.keys())
+    apps_groups.discard("additional_apps")
+    # apps_dict.update(apps_settings.additional_apps.dict().items())
+    all_variants_by_group_label = {}
+    for group_name in apps_groups:
+        app_group = getattr(apps_settings, group_name)
+        # Skip disabled group
+        if not app_group.enabled:
+            continue
+
+        # Skip group without variants
+        app_variants = list(app_group.variants)
+        if not app_variants:
+            continue
+
+        app_field = apps_fields[group_name]
+        group_label = app_field.field_info.title
+
+        app_variants = list(app_group.variants)
+        app_variants.sort(key=lambda x: x.label, reverse=True)
+        enum_variants = all_variants_by_group_label.setdefault(
+            group_label, []
+        )
+        enum_variants.extend([
+            {
+                "label": f"{group_label} {variant.label}",
+                "value": f"{group_name}/{variant.name}",
+            }
+            for variant in app_variants
+        ])
+
+    for additional_app in apps_settings.additional_apps:
+        group_name = additional_app.name
+        if not additional_app.enabled or not group_name:
+            continue
+
+        app_variants = list(additional_app.variants)
+        if not app_variants:
+            continue
+
+        group_label = additional_app.label
+        if not group_label:
+            group_label = group_name
+
+        app_variants.sort(key=lambda x: x.label, reverse=True)
+        enum_variants = all_variants_by_group_label.setdefault(
+            group_label, []
+        )
+        enum_variants.extend([
+            {
+                "label": f"{group_label} {variant.label}",
+                "value": f"{group_name}/{variant.name}",
+            }
+            for variant in app_variants
+        ])
+
+    all_variants = []
+    for key, value in sorted(all_variants_by_group_label.items()):
+        all_variants.extend(value)
+    return all_variants
+
+
+async def tools_enum(
+    project_name: str | None = None,
+    addon: BaseServerAddon = None,
+    settings_variant: str = None,
+):
+    if project_name is None:
+        settings = await addon.get_studio_settings(variant=settings_variant)
+    else:
+        settings = await addon.get_project_settings(
+            project_name=project_name, variant=settings_variant
+        )
+
+    enum_variants = []
+    for tool_group in settings.tool_groups:
+        group_label = tool_group.label
+        group_name = tool_group.name
+        enum_variants.extend([
+            {
+                "label": f"{group_label} {variant.label}",
+                "value": f"{group_name}/{variant.name}",
+            }
+            for variant in tool_group.variants
+        ])
+    return enum_variants
+
 
 def validate_json_dict(value):
     if not value.strip():
@@ -201,26 +331,45 @@ class ApplicationsSettings(BaseSettingsModel):
     @validator("additional_apps")
     def validate_unique_name(cls, value):
         ensure_unique_names(value)
+        for item in value:
+            if item.name in DEFAULT_APP_GROUPS:
+                raise BadRequestException(f"Duplicate name '{item.name}'")
         return value
 
 
 class ApplicationsAddonSettings(BaseSettingsModel):
+    only_available: bool = SettingsField(
+        True,
+        title="Show only available applications",
+        description=(
+            "Enable to show only applications in AYON Launcher for which"
+            " the executable paths are found on the running machine."
+            " This applies as an additional filter to the applications"
+            " defined in a  project's anatomy settings to ignore"
+            " unavailable applications."
+        )
+    )
+    project_applications: list[str] = SettingsField(
+        default_factory=list,
+        title="Applications",
+        description="Applications available in the project",
+        enum_resolver=applications_enum,
+    )
+    project_tools: list[str] = SettingsField(
+        default_factory=list,
+        title="Tools",
+        description="Tools available in the project",
+        enum_resolver=tools_enum,
+    )
     applications: ApplicationsSettings = SettingsField(
         default_factory=ApplicationsSettings,
-        title="Applications",
+        title="Application Definitions",
         scope=["studio"]
     )
     tool_groups: list[ToolGroupModel] = SettingsField(
         default_factory=list,
+        title="Tools Definitions",
         scope=["studio"]
-    )
-    only_available: bool = SettingsField(
-        True,
-        title="Show only available applications",
-        description="Enable to show only applications in AYON Launcher"
-                    " for which the executable paths are found on the running machine."
-                    " This applies as an additional filter to the applications defined in a "
-                    " project's anatomy settings to ignore unavailable applications."
     )
 
     @validator("tool_groups")
