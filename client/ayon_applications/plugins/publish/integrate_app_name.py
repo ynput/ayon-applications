166e8c669a26337ee66783113b26a2bfcf9719de
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-11-18 11:58:45 +0100

    add app name to version data

---
+++ client/ayon_applications/plugins/publish/integrate_app_name.py
@@ -0,0 +1,10 @@
+import pyblish.api
+
+
+class IntegrateAsset(pyblish.api.InstancePlugin):
+    """Add application name to version data."""
+    order = pyblish.api.IntegratorOrder - 0.49
+
+    def process(self, instance):
+        version_data = instance.data.setdefault("versionData", {})
+        version_data["ayon_app_name"] = instance.context.data["appName"]
