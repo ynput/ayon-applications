686e0431d94a8de438e51b00d7d256b14f889d50
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-02-16 17:32:16 +0100

    change formatting

--- server/constants.py
+++ server/constants.py
@@ -1,40 +1,148 @@
 INFO_BY_GROUP_NAME = {
-    "adsk_3dsmax": {"label": "3ds Max", "icon": "3dsmax.png"},
-    "aftereffects": {"label": "After Effects", "icon": "aftereffects.png"},
-    "blender": {"label": "Blender", "icon": "blender.png"},
-    "celaction": {"label": "Celaction 2D", "icon": "celaction.png"},
-    "cinema4d": {"label": "Cinema4D", "icon": "cinema4d.png"},
-    "clo": {"label": "CLO", "icon": "clo.png"},
-    "equalizer": {"label": "3DEqualizer", "icon": "3de4.png"},
-    "flame": {"label": "Flame", "icon": "flame.png"},
-    "fusion": {"label": "Fusion", "icon": "fusion.png"},
-    "gaffer": {"label": "Gaffer", "icon": "gaffer.png"},
-    "harmony": {"label": "Harmony", "icon": "harmony.png"},
-    "hiero": {"label": "Hiero", "icon": "hiero.png"},
-    "houdini": {"label": "Houdini", "icon": "houdini.png"},
-    "loki": {"label": "Loki", "icon": "loki.png"},
-    "maya": {"label": "Maya", "icon": "maya.png"},
-    "marmoset": {"label": "Marmoset Toolbag", "icon": "marmoset.png"},
-    "marvelousdesigner": {"label": "Marvelous Designer", "icon": "marvelousdesigner.png"},
-    "mochapro": {"label": "Mocha Pro", "icon": "mochapro.png"},
-    "motionbuilder": {"label": "Motion Builder", "icon": "motionbuilder.png"},
-    "nuke": {"label": "Nuke", "icon": "nuke.png"},
-    "nukeassist": {"label": "Nuke Assist", "icon": "nuke.png"},
-    "nukestudio": {"label": "Nuke Studio", "icon": "nukestudio.png"},
-    "nukex": {"label": "Nuke X", "icon": "nukex.png"},
-    "openrv": {"label": "OpenRV", "icon": "openrv.png"},
-    "photoshop": {"label": "Photoshop", "icon": "photoshop.png"},
-    "premiere": {"label": "Premiere", "icon": "premiere.png"},
-    "resolve": {"label": "Resolve", "icon": "resolve.png"},
-    "silhouette": {"label": "Silhouette", "icon": "silhouette.png"},
-    "substancepainter": {"label": "Substance Painter", "icon": "substancepainter.png"},
-    "substancedesigner": {"label": "Substance Designer", "icon": "substancedesigner.png"},
-    "speedtree": {"label": "Speedtree", "icon": "speedtree.png"},
-    "terminal": {"label": "Terminal", "icon": "terminal.png"},
-    "tvpaint": {"label": "TVPaint", "icon": "tvpaint.png"},
-    "unreal": {"label": "Unreal Editor", "icon": "ue4.png"},
-    "wrap": {"label": "Wrap", "icon": "wrap.png"},
-    "zbrush": {"label": "Zbrush", "icon": "zbrush.png"},
+    "adsk_3dsmax": {
+        "label": "3ds Max",
+        "icon": "3dsmax.png",
+    },
+    "aftereffects": {
+        "label": "After Effects",
+        "icon": "aftereffects.png",
+    },
+    "blender": {
+        "label": "Blender",
+        "icon": "blender.png",
+    },
+    "celaction": {
+        "label": "Celaction 2D",
+        "icon": "celaction.png",
+    },
+    "cinema4d": {
+        "label": "Cinema4D",
+        "icon": "cinema4d.png",
+    },
+    "clo": {
+        "label": "CLO",
+        "icon": "clo.png",
+    },
+    "equalizer": {
+        "label": "3DEqualizer",
+        "icon": "3de4.png",
+    },
+    "flame": {
+        "label": "Flame",
+        "icon": "flame.png",
+    },
+    "fusion": {
+        "label": "Fusion",
+        "icon": "fusion.png",
+    },
+    "gaffer": {
+        "label": "Gaffer",
+        "icon": "gaffer.png",
+    },
+    "harmony": {
+        "label": "Harmony",
+        "icon": "harmony.png",
+    },
+    "hiero": {
+        "label": "Hiero",
+        "icon": "hiero.png",
+    },
+    "houdini": {
+        "label": "Houdini",
+        "icon": "houdini.png",
+    },
+    "loki": {
+        "label": "Loki",
+        "icon": "loki.png",
+    },
+    "maya": {
+        "label": "Maya",
+        "icon": "maya.png",
+    },
+    "marmoset": {
+        "label": "Marmoset Toolbag",
+        "icon": "marmoset.png",
+    },
+    "marvelousdesigner": {
+        "label": "Marvelous Designer",
+        "icon": "marvelousdesigner.png",
+    },
+    "mochapro": {
+        "label": "Mocha Pro",
+        "icon": "mochapro.png",
+    },
+    "motionbuilder": {
+        "label": "Motion Builder",
+        "icon": "motionbuilder.png",
+    },
+    "nuke": {
+        "label": "Nuke",
+        "icon": "nuke.png",
+    },
+    "nukeassist": {
+        "label": "Nuke Assist",
+        "icon": "nuke.png",
+    },
+    "nukestudio": {
+        "label": "Nuke Studio",
+        "icon": "nukestudio.png",
+    },
+    "nukex": {
+        "label": "Nuke X",
+        "icon": "nukex.png",
+    },
+    "openrv": {
+        "label": "OpenRV",
+        "icon": "openrv.png",
+    },
+    "photoshop": {
+        "label": "Photoshop",
+        "icon": "photoshop.png",
+    },
+    "premiere": {
+        "label": "Premiere",
+        "icon": "premiere.png",
+    },
+    "resolve": {
+        "label": "Resolve",
+        "icon": "resolve.png",
+    },
+    "silhouette": {
+        "label": "Silhouette",
+        "icon": "silhouette.png",
+    },
+    "substancepainter": {
+        "label": "Substance Painter",
+        "icon": "substancepainter.png",
+    },
+    "substancedesigner": {
+        "label": "Substance Designer",
+        "icon": "substancedesigner.png",
+    },
+    "speedtree": {
+        "label": "Speedtree",
+        "icon": "speedtree.png",
+    },
+    "terminal": {
+        "label": "Terminal",
+        "icon": "terminal.png",
+    },
+    "tvpaint": {
+        "label": "TVPaint",
+        "icon": "tvpaint.png",
+    },
+    "unreal": {
+        "label": "Unreal Editor",
+        "icon": "ue4.png",
+    },
+    "wrap": {
+        "label": "Wrap",
+        "icon": "wrap.png",
+    },
+    "zbrush": {
+        "label": "Zbrush",
+        "icon": "zbrush.png",
+    },
 }
 
 
