4ba22efc5a9654b7178fb84024a2a6c238a718e7
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-04-13 14:56:02 +0200

    added helper functions to get actions available for an entity

--- client/ayon_applications/utils.py
+++ client/ayon_applications/utils.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 import copy
+from dataclasses import dataclass
 import json
 import platform
 import collections
@@ -13,7 +14,12 @@ import ayon_api
 
 from ayon_core import AYON_CORE_ROOT
 from ayon_core.settings import get_project_settings
-from ayon_core.lib import Logger, get_ayon_username, filter_profiles
+from ayon_core.lib import (
+    Logger,
+    get_ayon_username,
+    filter_profiles,
+    get_settings_variant,
+)
 from ayon_core.addon import AddonsManager
 from ayon_core.pipeline.template_data import get_template_data
 from ayon_core.pipeline.workfile import (
@@ -46,6 +52,8 @@ except ImportError:
 
 if typing.TYPE_CHECKING:
     from .defs import Application
+
+IDENTIFIER_PREFIX = "application.launch."
 
 
 def parse_environments(
@@ -289,6 +297,128 @@ def _get_app_full_names_from_settings(
             full_names.append(f"{group_name}/{variant_name}")
 
     return full_names
+
+
+@dataclass
+class ActionInfoContext:
+    project_name: str
+    entity_type: str
+    entity_ids: list[str]
+    entity_subtypes: list[str]
+
+
+@dataclass
+class ActionInfo:
+    context: ActionInfoContext
+    addon_name: str
+    addon_version: str
+    identifier: str
+    app_name: str
+    group_label: str
+    variant_label: str | None
+    icon: dict[str, Any] | None
+
+    @staticmethod
+    def from_webaction(
+        context: ActionInfoContext,
+        action: dict[str, Any],
+    ) -> "ActionInfo":
+        identifier = action["identifier"]
+        variant_label = action["label"]
+        group_label = action.get("groupLabel")
+        if not group_label:
+            group_label = variant_label or identifier
+            variant_label = None
+
+        app_name = identifier.removeprefix(IDENTIFIER_PREFIX)
+        return ActionInfo(
+            context=context,
+            addon_name=action["addonName"],
+            addon_version=action["addonVersion"],
+            identifier=identifier,
+            app_name=app_name,
+            group_label=group_label,
+            variant_label=variant_label,
+            icon=action["icon"],
+        )
+
+
+def get_applications_action_info_for_task(
+    project_name: str,
+    task_id: str,
+    task_type: str,
+) -> list[ActionInfo]:
+    """Helper function to get actions for a given task.
+
+    This function uses server side endpoint to resolve which actions are
+        available for a given project respecting project bundle.
+
+    Args:
+        project_name (str): Project entity.
+        task_id (str): Task id.
+        task_type (str): Task type.
+
+    Returns:
+        list[ActionInfo]: Applications actions available for the task.
+
+    """
+    variant = get_settings_variant()
+    context = ActionInfoContext(
+        project_name=project_name,
+        entity_type="task",
+        entity_ids=[task_id],
+        entity_subtypes=[task_type],
+    )
+    return [
+        ActionInfo.from_webaction(context, action)
+        for action in ayon_api.get_actions(
+            project_name,
+            entity_type=context.entity_type,
+            entity_ids=context.entity_ids,
+            entity_subtypes=context.entity_subtypes,
+            variant=variant,
+            mode="simple",
+        )
+        if action["identifier"].startswith(IDENTIFIER_PREFIX)
+    ]
+
+
+def get_applications_action_info_for_workfile(
+    project_name: str,
+    workfile_id: str,
+) -> list[ActionInfo]:
+    """Helper function to get actions for a given workfile.
+
+    This function uses server side endpoint to resolve which actions are
+        available for a given project respecting project bundle.
+    
+    Args:
+        project_name (str): Project entity.
+        workfile_id (str): Workfile id.
+
+    Returns:
+        list[ActionInfo]: Applications actions available for the workfile.
+
+    """
+    variant = get_settings_variant()
+    context = ActionInfoContext(
+        project_name=project_name,
+        entity_type="workfile",
+        entity_ids=[workfile_id],
+        entity_subtypes=[],
+    )
+    return [
+        ActionInfo.from_webaction(context, action)
+        for action in ayon_api.get_actions(
+            project_name,
+            entity_type=context.entity_type,
+            entity_ids=context.entity_ids,
+            entity_subtypes=context.entity_subtypes,
+            variant=variant,
+            mode="dynamic",
+        )
+        if action["identifier"].startswith(IDENTIFIER_PREFIX)
+    ]
 
 
 def get_applications_for_context(
