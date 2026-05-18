5930e5ca6ef761ff3560e47a5ee5311d01dd0227
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-05-18 17:07:13 +0200

    remove deprecated settings

--- server/settings.py
+++ server/settings.py
@@ -520,15 +520,6 @@ class ProjectToolsProfile(BaseSettingsModel):
 
 
 class ProjectApplicationsModel(BaseSettingsModel):
-    enabled: bool = SettingsField(
-        True,
-        title="Use Applications profiles instead of attribute",
-        description=(
-            "Use applications attribute on the project instead of these"
-            " profiles. Attribute based applications will"
-            " be deprecated in future versions of applications addon."
-        ),
-    )
     profiles: list[ProjectApplicationsProfile] = SettingsField(
         default_factory=list,
         title="Profiles",
@@ -536,15 +527,6 @@ class ProjectApplicationsModel(BaseSettingsModel):
 
 
 class ProjectToolsModel(BaseSettingsModel):
-    enabled: bool = SettingsField(
-        True,
-        title="Use Tools profiles instead of attribute",
-        description=(
-            "Use tools attribute on folders and tasks instead of these"
-            " profiles. Attribute based tools will"
-            " be deprecated in future versions of applications addon."
-        ),
-    )
     profiles: list[ProjectToolsProfile] = SettingsField(
         default_factory=list,
         title="Profiles",
@@ -610,7 +592,6 @@ def _get_tools_defaults():
 
 
 DEFAULT_VALUES = {
-    "only_available": True,
     "project_applications": {
         "profiles": [
             {
